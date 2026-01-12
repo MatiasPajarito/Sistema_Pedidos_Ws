1a28071995aa88798bf3181f5e4903800f18f8dd
Matias Pajarito <116607344+MatiasPajarito@users.noreply.github.com>
2026-01-12 19:44:30 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import DemoCaburguau from './DemoCaburguau' 
-import './index.css' // Esta línea es la que trae toda la elegancia
+import './index.css'  <-- COMENTA ESTA LÍNEA si aún no creas el archivo index.css en src
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
