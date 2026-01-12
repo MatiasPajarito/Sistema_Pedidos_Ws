8c1da0d79099402343fc2c6d4db177c66ac89ffd
Matias Pajarito <116607344+MatiasPajarito@users.noreply.github.com>
2026-01-12 19:43:02 -0300

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DemoCaburguau from './DemoCaburguau' 
+// import './index.css'  <-- COMENTA ESTA LÍNEA si aún no creas el archivo index.css en src
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DemoCaburguau />
+  </React.StrictMode>,
+)
