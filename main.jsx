f4c58e58293dfc46954dffafc36c6ab08c059916
Matias Pajarito <116607344+MatiasPajarito@users.noreply.github.com>
2026-01-12 19:23:44 -0300

    Replace App component with DemoCaburguau
    Updated main.jsx to use DemoCaburguau component instead of App.

--- main.jsx
+++ main.jsx
@@ -1,24 +1,10 @@
-/*import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-*/
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'  <-- COMENTA ESTA LÍNEA (ponle // al principio)
-import DemoCaburguau from './DemoCaburguau.jsx' // <-- AGREGA ESTA LÍNEA
-import './index.css'
+import DemoCaburguau from './DemoCaburguau.jsx' // Importación local directa
+import './index.css' // Si no tienes index.css, puedes borrar esta línea para evitar más errores
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App />  <-- COMENTA ESTA */}
-    <DemoCaburguau /> {/* <-- AGREGA ESTA */}
+    <DemoCaburguau />
   </React.StrictMode>,
 )
