02e6358a47b3cf25b6f245ec807db561331168bb
Matias Pajarito <116607344+MatiasPajarito@users.noreply.github.com>
2026-01-12 19:39:19 -0300

    Replace App component with DemoCaburguau

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
-/*import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-*/
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'  <-- COMENTA ESTA LÍNEA (ponle // al principio)
-import DemoCaburguau from './DemoCaburguau' // <-- AGREGA ESTA LÍNEA
+import DemoCaburguau from './DemoCaburguau' 
+import './index.css' // Esta línea es la que trae toda la elegancia
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App />  <-- COMENTA ESTA */}
-    <DemoCaburguau /> {/* <-- AGREGA ESTA */}
+    <DemoCaburguau />
   </React.StrictMode>,
 )
